c155339872c29acf1253188807be9c36ead76c78
Srinath Warrier <srinath.warrier@sap.com>
2015-08-09 00:06:36 +0530

    getAllOnSale added

--- config/routes.js
+++ config/routes.js
@@ -64,7 +64,9 @@ module.exports.routes = {
   'get /v2/getOutletDetails':'V2Controller.getOutletDetails',
   'get /v2/getRelatedBrandOutlets':'V2Controller.getRelatedBrandOutlets',
   'get /v2/getTakeMeThereCommands' :'V2Controller.getTakeMeThereCommands',
-  'get /v2/getAllFavoriteOutlets' : 'V2Controller.getAllFavoriteOutlets'
+  'get /v2/getAllFavoriteOutlets' : 'V2Controller.getAllFavoriteOutlets',
+  'get /v2/getOutletsForTagId' : 'V2Controller.getOutletsForTagId',
+  'get /v2/setFavoriteOutlet' : 'V2Controller.setFavoriteOutlet'
 
 
 
